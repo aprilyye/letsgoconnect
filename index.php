44fa3a9c8faf1438387477a0bcb04b6d90655dd6
April Ye <yy459@cornell.edu>
2018-07-07 23:20:40 -0700

    LGG-2 add bio to pics

--- index.php
+++ index.php
@@ -17,12 +17,22 @@
 </head>
 
 <body>
-  <?php include("includes/header.php");?>
+  <?php
+    include("includes/header.php");
+  ?>
 
   <?php
-    $sql = "SELECT id, folder_path, file_name FROM img ORDER BY id";
+    // $sql = "SELECT id, folder_path, file_name FROM img ORDER BY id";
+    // $params = array();
+    // $imgs = exec_sql_query($db, $sql, $params)->fetchAll();
+    // echo "reached here".$imgs;
+    // display_all_pics($imgs);
+
+    // query to display all images from database
+    $sql = "SELECT id, folder_path, file_name, player_name, bio, kgs_id FROM img ORDER BY id";
     $params = array();
     $imgs = exec_sql_query($db, $sql, $params)->fetchAll();
+
     if (isset($imgs) and !empty($imgs)) {
         echo "<table>";
         echo "<tr>";
@@ -30,8 +40,15 @@
         foreach($imgs as $img) {
             echo "<td>";
             $src = $img[1].$img[2];
-            echo '<a href="index.php?image='.$img[0].'"><img alt="Image Upload" src="'.$src.'" width="300" height="335"/></a>';
-
+            echo '<a href="index.php?image='.$img[0].'"><img alt="Image Upload"
+                  src="'.$src.'" width="350" height="335"/></a>';
+            $player_name = $img[3];
+            $bio = $img[4];
+            $kgs_id = $img[5];
+            echo nl2br("\n".$player_name."\n".$bio);
+            if (!empty($kgs_id)) {
+              echo nl2br("\n Find me on KGS: ".$kgs_id);
+            }
             echo "</td>";
             $counter++;
             if ($counter % 4 == 0) {
